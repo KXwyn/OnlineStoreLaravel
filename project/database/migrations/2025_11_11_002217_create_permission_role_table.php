bfa645dfa490635b3b76d2b09cc003c159a71af1
stevenbedoya08 <stevenbedoyaortiz@gmail.com>
2025-11-10 23:49:26 -0500

    2025_11_11_002217_create_permission_role_table.php

--- project/database/migrations/2025_11_11_002217_create_permission_role_table.php
+++ project/database/migrations/2025_11_11_002217_create_permission_role_table.php
@@ -4,28 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePermissionRoleTable extends Migration
+return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('permission_role', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('permission_id')->constrained()->onDelete('cascade');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('permission_role');
     }
-}
+};
