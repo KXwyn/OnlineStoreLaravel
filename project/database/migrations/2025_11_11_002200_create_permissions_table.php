e526f647d34be398ae78a19dd96421534661f721
Salv313 <99740836+Salv313@users.noreply.github.com>
2025-11-10 20:02:38 -0500

    Refactor CreatePermissionsTable migration class

--- project/database/migrations/2025_11_11_002200_create_permissions_table.php
+++ project/database/migrations/2025_11_11_002200_create_permissions_table.php
@@ -4,27 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePermissionsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+retun new class extends Migration
+ { 
+    public function up(): void 
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
+            
+    $table->string('name')->unique();
+            $table->string('description');
+            $table->string('module');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down():void
     {
         Schema::dropIfExists('permissions');
     }
