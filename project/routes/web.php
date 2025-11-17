0c5762dbcd0b78b39dd37b267acf79a8ea75a018
KXwyn <stmiguelwyn@gmail.com>
2025-11-17 18:03:07 -0500

    Add Laravel IDE Helper

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +12,17 @@ Route::get('/blogs', [BlogController::class, 'index'])
 
 Route::post('/blogs', [BlogController::class, 'store'])
     ->middleware('permission:createBlogs');
+
+//Rutas de autenticación
+    // Mostrar el formulario de registro
+Route::get('/register', [LoginController::class, 'showRegisterForm'])->name('register');
+// Procesar el formulario de registro
+Route::post('/register', [LoginController::class, 'register']);
+
+// Mostrar el formulario de login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+// Procesar el formulario de login
+Route::post('/login', [LoginController::class, 'login']);
+
+// Cerrar sesión
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
