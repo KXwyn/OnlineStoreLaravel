7259504b2f83f2cec36dec8cde47d14938312a47
KXwyn <stmiguelwyn@gmail.com>
2025-11-17 20:39:03 -0500

    Integrate UI and initial views

--- project/app/Http/Controllers/BlogController.php
+++ project/app/Http/Controllers/BlogController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    /**
+     * Muestra la página principal de la sección de Blogs.
+     */
+    public function index()
+    {
+        // En el futuro aquí obtendrás los blogs de la base de datos,
+        // pero por ahora, solo mostramos la vista.
+        return view('blogs.index');
+    }
 }
---
+++ project/resources/views/auth/login.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <a href="/" class="h1"><b>Mi Tienda</b></a>
+        </div>
+        <div class="card-body">
+            <p class="login-box-msg">Inicia sesión para continuar</p>
+
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="input-group mb-3">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required>
+                    <div class="input-group-append">
+                        <div class="input-group-text"><span class="fas fa-envelope"></span></div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" name="password" class="form-control" placeholder="Password" required>
+                    <div class="input-group-append">
+                        <div class="input-group-text"><span class="fas fa-lock"></span></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary btn-block">Iniciar Sesión</button>
+                    </div>
+                </div>
+            </form>
+            <p class="mb-0">
+                <a href="{{ route('register') }}" class="text-center">Registrar una nueva cuenta</a>
+            </p>
+        </div>
+    </div>
+</div>
+<script src="{{ asset('vendor/adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+</body>
+</html>
